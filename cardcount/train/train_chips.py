00896339eb7687dc22ad1ade1185b18177f00326
jet locati <jetlocati4922@gmail.com>
2026-08-16 19:45:16 -0600

    Complete chip testing

--- cardcount/train/train_chips.py
+++ cardcount/train/train_chips.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from ultralytics import YOLO
 
-YMAL_DATA = Path("data/datasets/chips/data.yaml")
+YMAL_DATA = r"C:\Users\jetlo\onedrive\documents\github\ALAN\cardcount\data/datasets/chips/data.yaml"
 
 
 
@@ -11,7 +11,7 @@ def main() -> None:
     model.train(
         data=str(YMAL_DATA), project="runs/chips", name="y11n_640_v1",
         epochs=60, patience=20, batch=-1, 
-        migsz=640, hsv_h=0.0, hsv_s=0.4, hsv_v=0.4, degrees=180.0,fliplr=0.5, flipud=0.5, scale=0.4, mosaic=1.0, close_mosaic=15, mixup=0.0,
+        imgsz=640, hsv_h=0.0, hsv_s=0.4, hsv_v=0.4, degrees=180.0,fliplr=0.5, flipud=0.5, scale=0.4, mosaic=1.0, close_mosaic=15, mixup=0.0,
         cos_lr=True, seed=0, plots=True
     )
     metrics = model.val(data=str(YMAL_DATA), split="test")
