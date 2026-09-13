c19499a44fdae6129ea1d99fdc8ad00035e2aeed
jet locati <jetlocati4922@gmail.com>
2026-09-13 02:47:00 -0600

    Update PredictBestPlay.py

--- cardcount/logic/PredictBestPlay.py
+++ cardcount/logic/PredictBestPlay.py
@@ -3,26 +3,28 @@ from cardcount.vision_detection.ChipVision import DENOMINATIONS
 from cardcount.vision_detection.CardVison import BJ_COUNTS
 
 #find likley move that would be charictaristic of a normal player
-def FindLikleyMoveNorm(DealerCards, PlayeCards) -> :
-    DealerHandValue = 0
-    PlayerHandValue = 0
-    Assumed = False
+def FindLikleyMoveNorm(RoundProgression, DealerHandValue, PlayeHandValue, HandType, D_ace) -> str:
+    if RoundProgression == "ROUND IN PROGRESS":
+        if HandType == "HARD":
+            if (PlayeHandValue[0] <= 11 and HandType == "HARD"):
+                return "HIT"
 
-    if len(DealerCards) == 1:
-        DealersCard = DealerCards.get(DealerCards[0])
-        DealersCard  = BJ_COUNTS.get(DealersCard)
-        DealerHandValue = DealersCard + 10
-        Assumed = True
+            if ((PlayeHandValue[0] == 12) and ( 4 <= DealerHandValue[0] <= 6)):
+                return "HIT"
 
-    else:
-        for i in DealerCards:
-            DealerHandValue += BJ_COUNTS(i, 0)
+            if ((13<= PlayeHandValue[0] <=16 ) and not (2<= DealerHandValue <=6)):
+                return "HIT"
 
-    for i in PlayeCards:
-        PlayerHandValue += BJ_COUNTS.get(i, 0)
+            if (PlayeHandValue[0] >= 17):
+                return "HIT"
 
-    PlayerDealerDiff = PlayerHandValue - DealerHandValue
+            return "STAND"
+        if (PlayeHandValue[1] <= 17):
+            return "HIT"
+        if ((PlayeHandValue[1] == 18) and (DealerHandValue[1] == 9 or DealerHandValue[1] == 10 or (D_ace)) or (3<= DealerHandValue[1] <=6 )):
+            return "HIT"
 
+        return "STAND"
     
 
 
