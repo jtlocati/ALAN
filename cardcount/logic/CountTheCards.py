20f07ad0ba271aa07780277a43be44ecd10377e9
jet locati <jetlocati4922@gmail.com>
2026-09-13 23:00:03 -0600

    card counting logic

---
+++ cardcount/logic/CountTheCards.py
@@ -0,0 +1,21 @@
+#Takes an infence of all cards on the table in order to return
+
+def HandCount(CardPlayer, CardDealer):
+    COUNT = 0
+
+    #Calculate players weight first
+    for i in CardPlayer:
+        try:
+            Value = int(i)
+
+            if (2 <= Value < 6):
+                COUNT+=1
+            #eXPRESS STEP FOR READABILITY
+            elif (6 <= Value >= 7):
+                COUNT += 0
+            else:
+                COUNT -= 1
+        except ValueError:
+            COUNT -= 1
+
+    return COUNT
